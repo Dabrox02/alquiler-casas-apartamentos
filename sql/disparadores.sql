02d7d51a6c0e34d04365d324720a00031e2d523e
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2023-11-23 22:43:46 -0500

    feat: :card_file_box: add new triggers

--- sql/disparadores.sql
+++ sql/disparadores.sql
@@ -1,9 +1,9 @@
 
 -- VALIDAR valorxNoche sea positivo.
 
-DROP TRIGGER IF EXISTS ins_propiedad_check_valorxNoche;
+DROP TRIGGER IF EXISTS bef_ins_check_valorxNoche;
 DELIMITER //
-CREATE TRIGGER ins_check_valorxNoche 
+CREATE TRIGGER bef_ins_check_valorxNoche 
 BEFORE INSERT ON propiedad
 FOR EACH ROW
 BEGIN
@@ -15,9 +15,9 @@ END;//
 DELIMITER ;
 
 
-DROP TRIGGER IF EXISTS upd_propiedad_check_valorxNoche;
+DROP TRIGGER IF EXISTS bef_upd_propiedad_check_valorxNoche;
 DELIMITER //
-CREATE TRIGGER upd_propiedad_check_valorxNoche 
+CREATE TRIGGER bef_upd_propiedad_check_valorxNoche 
 BEFORE UPDATE ON propiedad
 FOR EACH ROW
 BEGIN
@@ -29,9 +29,9 @@ END;//
 DELIMITER ;
 
 
-DROP TRIGGER IF EXISTS ins_detallePropiedad_check;
+DROP TRIGGER IF EXISTS bef_ins_detallePropiedad_check;
 DELIMITER //
-CREATE TRIGGER ins_detallePropiedad_check 
+CREATE TRIGGER bef_ins_detallePropiedad_check 
 BEFORE INSERT ON detallePropiedad
 FOR EACH ROW
 BEGIN
@@ -52,9 +52,9 @@ END;//
 DELIMITER ;
 
 
-
+DROP TRIGGER IF EXISTS bef_upd_detallePropiedad_check;
 DELIMITER //
-CREATE TRIGGER upd_detallePropiedad_check 
+CREATE TRIGGER bef_upd_detallePropiedad_check 
 BEFORE UPDATE ON detallePropiedad
 FOR EACH ROW
 BEGIN
@@ -73,3 +73,81 @@ BEGIN
     END IF;
 END;//
 DELIMITER ;
+
+
+DROP TRIGGER IF EXISTS bef_ins_reserva_check;
+DELIMITER //
+CREATE TRIGGER bef_ins_reserva_check
+BEFORE INSERT ON reserva
+FOR EACH ROW
+BEGIN
+    IF NEW.fechaReserva > NEW.fechaEntrada THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'La fecha de reserva debe ser menor o igual a la fecha de entrada.';
+    END IF;
+    IF NEW.diasEstancia <= 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Los días de estancia deben ser mayores a 0.';
+    END IF;
+END;
+//
+DELIMITER ;
+
+
+DROP TRIGGER IF EXISTS bef_upd_reserva_check;
+DELIMITER //
+CREATE TRIGGER bef_ins_reserva_check
+BEFORE UPDATE ON reserva
+FOR EACH ROW
+BEGIN
+    IF NEW.fechaReserva > NEW.fechaEntrada THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'La fecha de reserva debe ser menor o igual a la fecha de entrada.';
+    END IF;
+    IF NEW.diasEstancia <= 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Los días de estancia deben ser mayores a 0.';
+    END IF;
+END;
+//
+DELIMITER ;
+
+
+DROP TRIGGER IF EXISTS bef_ins_pago_check;
+DELIMITER //
+CREATE TRIGGER bef_ins_pago_check 
+BEFORE INSERT ON pago
+FOR EACH ROW
+BEGIN
+	SET @valorxNochePropiedad = COALESCE((SELECT pro.valorxNoche FROM reserva re, propiedad pro WHERE re.idPropiedad = pro.idPropiedad AND re.idReserva = NEW.idReserva), 0);
+    SET @diasEstanciaReserva = COALESCE((SELECT re.diasEstancia FROM reserva re WHERE re.idReserva = NEW.idReserva), 0);
+	SET @fechaReserva = (SELECT re.fechaReserva FROM reserva re WHERE re.idReserva = NEW.idReserva);
+    SET NEW.valorPago = @valorxNochePropiedad * @diasEstanciaReserva;
+		
+	IF @fechaReserva > NEW.fechaPago THEN
+		SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'No se puede generar un antes de una reserva.';
+    END IF;
+END;//
+DELIMITER ;
+
+
+DROP TRIGGER IF EXISTS bef_upd_pago_check;
+DELIMITER //
+CREATE TRIGGER bef_upd_pago_check 
+BEFORE UPDATE ON pago
+FOR EACH ROW
+BEGIN
+	SET @valorxNochePropiedad = COALESCE((SELECT pro.valorxNoche FROM reserva re, propiedad pro WHERE re.idPropiedad = pro.idPropiedad AND re.idReserva = NEW.idReserva), 0);
+    SET @diasEstanciaReserva = COALESCE((SELECT re.diasEstancia FROM reserva re WHERE re.idReserva = NEW.idReserva), 0);
+	SET @fechaReserva = (SELECT re.fechaReserva FROM reserva re WHERE re.idReserva = NEW.idReserva);
+    SET NEW.valorPago = @valorxNochePropiedad * @diasEstanciaReserva;
+		
+	IF @fechaReserva > NEW.fechaPago THEN
+		SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'No se puede generar un pago antes de una reserva.';
+    END IF;
+END;//
+DELIMITER ;
+
+
